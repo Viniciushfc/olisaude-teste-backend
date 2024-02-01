5efe0b772d2c8d4206e6275d5368b7894e80ddf3
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-01 18:16:13 -0300

    Inicial Commit

---
+++ src/main/java/br/com/example/viniciushfc/olisaude/OlisaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.viniciushfc.olisaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlisaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlisaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/viniciushfc/olisaude/OlisaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.viniciushfc.olisaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlisaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
