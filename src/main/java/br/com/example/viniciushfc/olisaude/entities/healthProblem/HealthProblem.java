8951f8136a13856e2588c772bd5c8bcb9e60dac8
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-03 15:32:15 -0300

    Retirado Tags com relação a JPA e adicionado anotação @Embeddable.

--- src/main/java/br/com/example/viniciushfc/olisaude/entities/healthProblem/HealthProblem.java
+++ src/main/java/br/com/example/viniciushfc/olisaude/entities/healthProblem/HealthProblem.java
@@ -1,30 +1,19 @@
 package br.com.example.viniciushfc.olisaude.entities.healthProblem;
 
-import br.com.example.viniciushfc.olisaude.dto.HealthProblemDTO;
-import jakarta.persistence.*;
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "healthProblems")
-@Table(name = "healthProblems")
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class HealthProblem {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String diseaseName;
-    @Column(nullable = true)
     private Integer degree;
-
-    public HealthProblem(HealthProblemDTO dto) {
-        this.diseaseName = dto.diseaseName();
-        this.degree = dto.degree();
-    }
 
 }
