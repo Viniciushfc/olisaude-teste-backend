9d98dae3672bcdb3c05f57b6b0c0903bdd0f12b2
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-02-03 20:24:20 -0300

    renomeado interface de repository.

--- src/main/java/br/com/example/viniciushfc/olisaude/infra/repository/ClientRepository.java
+++ src/main/java/br/com/example/viniciushfc/olisaude/infra/repository/ClientRepository.java
@@ -3,5 +3,5 @@ package br.com.example.viniciushfc.olisaude.infra.repository;
 import br.com.example.viniciushfc.olisaude.entities.client.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
